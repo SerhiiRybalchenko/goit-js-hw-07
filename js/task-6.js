692bf37b21b56dbac9edb7f9ed108d641b5120be
SerhiiRybalchenko <s.rybalchenko@ukr.net>
2024-03-17 12:51:18 +0100

    js

--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,43 @@
-const controls = document.getElementById('controls');
-const input = controls.querySelector('input');
-const createButton = controls.querySelector('button[data-action="create"]');
-const destroyButton = controls.querySelector('button[data-action="destroy"]');
+const input = document.querySelector('input[type="number"]');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
 const boxesContainer = document.getElementById('boxes');
 
-createButton.addEventListener('click', createBoxes);
-destroyButton.addEventListener('click', destroyBoxes);
+createButton.addEventListener('click', handlerClick);
+destroyButton.addEventListener('click', handlerClick);
 
-function createBoxes() {
-  const amount = Number(input.value);
-  if (amount < 1 || amount > 100 || isNaN(amount)) {
-    return;
+function handlerClick(e) {
+  let amount;
+  if (e.currentTarget.dataset.create !== undefined) {
+    destroyBoxes();
+    amount = Number(input.value);
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+    }
+    input.value = '';
   }
-
-  destroyBoxes(); 
-
-  let size = 30;
-
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-    
-    size += 10;
+  if (e.currentTarget.dataset.destroy !== undefined) {
+    destroyBoxes();
   }
-
-  input.value = '';
 }
 
 function destroyBoxes() {
   boxesContainer.innerHTML = '';
 }
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = ${size}px;
+    box.style.height = ${size}px;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
